6130f286ede0f0f7bb302cfacc1ad4fb777cfb06
dimildesigner <dimildesignerdev@gmail.com>
2026-03-20 14:04:42 -0300

    Arquivos do exercicio 2, contagem regressiva (com foguete decolando)

--- js_for_exercicio1.js
+++ js_for_exercicio1.js
@@ -1,4 +1,4 @@
-// EXERCICIOS FOR
+// EXERCICIO FOR
 // 1- Você foi solicitado a criar uma aplicação que exibe 
 // a tabuada de um número fornecido pelo usuário. 
 // O programa deve solicitar um número e exibir a tabuada 
@@ -9,19 +9,14 @@
 const prompt = require('prompt-sync')()
 
 console.log("\n-**** Tabuada ****-\n")
-// Usamos Number() ou parseInt() pois tabuada geralmente é com números inteiros
+
 let numero = Number(prompt('Digite um número para ver a tabuada: '));
 
 console.log("\n" + `A tabuada de ${numero} é: `)
 
-// 1. Começamos o i em 1
-// 2. Vamos até o 10
-// 3. Somamos +1 a cada volta (i++)
-for (let i = 1; i <= 10; i++) {
-  // Criamos uma variável para o resultado para o código ficar mais legível
+for (let i = 1; i <= 10; i++) {  
   let resultado = numero * i;
   
-  // Usamos Template Strings (com crase) para exibir bonitinho: "5 x 1 = 5"
   console.log(`${numero} x ${i} = ${resultado}`);
 }
 
---
+++ js_for_exercicio2.js
@@ -0,0 +1,23 @@
+// EXERCICIO FOR
+// O loop for tem 3 partes: (início; condição; passo)
+// Você foi encarregado de desenvolver um programa que realiza uma contagem regressiva. 
+// Utilize um loop for para contar de 10 a 1 e imprimir cada número na tela. 
+// Como você pode fazer isso?
+
+console.log("\n->>>> Contagem Regressiva <<<<-\n")
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
+console.log("                                🚀\n\n\n\n");
+console.log("                       ´\n\n");
+console.log("                   ´");
+console.log("           ||===  ´");
+console.log("           ||    ´");
+console.log("           ||   ()");
+console.log("           ||  (())");
+console.log("           || ((()))");
+console.log("Decolar!   ||(((())))");
+console.log("          -HHHHHHHHHHHH-\n");
+
+console.log("-------------\n");
